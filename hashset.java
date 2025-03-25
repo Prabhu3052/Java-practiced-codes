c35e59c777b265dd71d2be6c0a62353444c89484
Prabhu3052 <prabhu23lakshmi@gmail.com>
2025-03-25 10:57:20 +0530

    final commit

---
+++ hashset.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class hashset{
+    public static void main(String [] args){
+        HashSet<Integer> set = new HashSet<Integer>();
+        set.add(76);
+        set.add(12);
+        set.add(45);
+        set.add(77);
+        set.add(13);
+        set.add(99);
+        for (int i=0;i<=100;i++){
+            if (set.contains(i)){
+                System.out.println(i+" is in the set");
+            }
+            else{
+                System.out.println(i+" is not in the set");
+            }
+
+        }
+    }
+}
